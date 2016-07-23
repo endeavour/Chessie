9c14f2193ffaa0599430c9d98dbe45571d0242f4
James Freiwirth <james@perfectshuffle.co.uk>
2016-07-23 13:02:08 +0100

    Added some extension methods

---
+++ src/Chessie/ChessieExtensions.fs
@@ -0,0 +1,24 @@
+﻿/// Contains additional error propagation functions and a computation expression builder for Railway-oriented programming.
+namespace Chessie.ErrorHandling.Extensions
+
+open System
+open Chessie.ErrorHandling
+
+module Trial =
+  let failIfFalse e flag =
+    if flag then ok () else fail e
+
+module AsyncTrial =
+  open Chessie.ErrorHandling
+  
+  let catch (x:Async<'a>) : AsyncResult<'a, exn, _> =
+    async {
+      let! wrapped = x |> Async.Catch 
+      return Trial.ofChoice wrapped
+    } |> AR
+
+  let lift f = Async.ofAsyncResult >> Async.map f >> AR
+
+  let mapSuccess f = lift (Trial.lift f)
+
+  let mapFailure f = lift (Trial.mapFailure f)
